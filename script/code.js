f1f66ad2c69979ebd467e2a760b9fc6a1e594e71
KgMLe <kgodisholebopa@gmail.com>
2023-05-17 15:11:48 +0200

    JS_Day4

---
+++ script/code.js
@@ -0,0 +1,20 @@
+let person1 = {
+    name: 'Kgodisho',
+    age:24,
+    city: 'Cape Town'
+     
+}
+
+console.log(person1);
+
+let person2 ={
+    name: 'Nkgudisheng',
+    age: 28,
+    city: 'Midrand'
+}
+
+console.log(person2);
+
+let people =[{name:'Kgodisho',age: 24, city:'Cape Town'}, {name:'Nkgudisheng', age:28, city:'Midrand'}]
+
+console.log(people);
